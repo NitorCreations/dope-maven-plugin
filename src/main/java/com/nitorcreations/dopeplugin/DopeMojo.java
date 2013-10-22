d204a93841601e05fda328a718b4e5280fa35e62
Pasi Niemi <ext-pasi.1.niemi@nokia.com>
2013-10-22 13:16:35 +0300

    Skip pygments for verbatim blocks without type

--- src/main/java/com/nitorcreations/dopeplugin/DopeMojo.java
+++ src/main/java/com/nitorcreations/dopeplugin/DopeMojo.java
@@ -539,22 +539,26 @@ public class DopeMojo extends AbstractMojo {
 		@Override
 		public void visit(VerbatimNode node) {
 			try {
-				synchronized (this.getClass()) {
-					PythonInterpreter interpreter = new PythonInterpreter();
-					String lang = WordUtils.capitalize(node.getType());
-					interpreter.set("code", node.getText());
+				if (!node.getType().isEmpty()) {
+					synchronized (this.getClass()) {
+						PythonInterpreter interpreter = new PythonInterpreter();
+						String lang = WordUtils.capitalize(node.getType());
+						interpreter.set("code", node.getText());
 
-					interpreter.exec("from pygments import highlight\n"
-							+ "from pygments.lexers import " + lang + "Lexer\n"
-							+ "from pygments.formatters import HtmlFormatter\n"
-							+ "\nresult = highlight(code, " + lang + "Lexer(), HtmlFormatter())");
+						interpreter.exec("from pygments import highlight\n"
+								+ "from pygments.lexers import " + lang + "Lexer\n"
+								+ "from pygments.formatters import HtmlFormatter\n"
+								+ "\nresult = highlight(code, " + lang + "Lexer(), HtmlFormatter())");
 
-					String ret = interpreter.get("result", String.class); 
-					if (ret != null) {
-						printer.print(ret);
-					} else {
-						super.visit(node);
+						String ret = interpreter.get("result", String.class); 
+						if (ret != null) {
+							printer.print(ret);
+						} else {
+							super.visit(node);
+						}
 					}
+				} else {
+					super.visit(node);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
